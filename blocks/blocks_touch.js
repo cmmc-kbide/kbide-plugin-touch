c007e59458e390382a87f04966b201c04318975d
Prawared Bowonphattharawadi <prawared.bw@gmail.com>
2019-08-26 18:06:00 +0700

    add generator

--- blocks/blocks_touch.js
+++ blocks/blocks_touch.js
@@ -1,18 +1,26 @@
-Blockly.Blocks['touch_block'] = {
-  init: function() {
-    this.appendDummyInput()
-        .appendField("TOUCH");
-    this.appendValueInput("TOUCH_BLOCK_ANALOG_PIN")
-        .setCheck("Number")
-        .appendField("ANALOG PIN");
-    this.appendValueInput("TOUCH_BLOCK_DIGITAL_PIN")
-        .setCheck("Number")
-        .appendField("DIGITAL PIN");
-    this.setInputsInline(true);
-    this.setPreviousStatement(true, null);
-    this.setNextStatement(true, null);
-    this.setColour(230);
- this.setTooltip("");
- this.setHelpUrl("");
-  }
-};
+Blockly.Blocks["touch_block"] = {
+    init: function() {
+      this.appendDummyInput().appendField("TOUCH");
+      this.appendDummyInput()
+        .appendField("ANALOG PIN")
+        .appendField(new Blockly.FieldTextInput("A0"), "TOUCH_ANALOG_PIN")
+        .appendField("SET TO")
+        .appendField(
+          new Blockly.FieldVariable("TOUCH_ANALOG_VALUE"),
+          "TOUCH_ANALOG_VALUE"
+        );
+      this.appendDummyInput()
+        .appendField("DIGITAL PIN")
+        .appendField(new Blockly.FieldTextInput("0"), "TOUCH_DIGITAL_PIN")
+        .appendField("SET TO")
+        .appendField(
+          new Blockly.FieldVariable("TOUCH_DIGITAL_VALUE"),
+          "TOUCH_DIGITAL_VALUE"
+        );
+      this.setPreviousStatement(true, null);
+      this.setNextStatement(true, null);
+      this.setColour(230);
+      this.setTooltip("");
+      this.setHelpUrl("");
+    }
+  };
--- blocks/generators_touch.js
+++ blocks/generators_touch.js
@@ -1,7 +1,33 @@
-Blockly.JavaScript['touch_block'] = function(block) {
-  var value_touch_block_analog_pin = Blockly.JavaScript.valueToCode(block, 'TOUCH_BLOCK_ANALOG_PIN', Blockly.JavaScript.ORDER_ATOMIC);
-  var value_touch_block_digital_pin = Blockly.JavaScript.valueToCode(block, 'TOUCH_BLOCK_DIGITAL_PIN', Blockly.JavaScript.ORDER_ATOMIC);
-  // TODO: Assemble JavaScript into code variable.
-  var code = '...;\n';
-  return code;
-};
+Blockly.JavaScript["touch_block"] = function(block) {
+    var text_touch_analog_pin = block.getFieldValue("TOUCH_ANALOG_PIN");
+    var variable_touch_analog_value = Blockly.JavaScript.variableDB_.getName(
+      block.getFieldValue("TOUCH_ANALOG_VALUE"),
+      Blockly.Variables.NAME_TYPE
+    );
+    var text_touch_digital_pin = block.getFieldValue("TOUCH_DIGITAL_PIN");
+    var variable_touch_digital_value = Blockly.JavaScript.variableDB_.getName(
+      block.getFieldValue("TOUCH_DIGITAL_VALUE"),
+      Blockly.Variables.NAME_TYPE
+    );
+    // TODO: Assemble JavaScript into code variable.
+    var code = `
+    #VARIABLE
+	// Declaration and initialization of the input pin
+	int TOUCH_ANALOG_PIN = ${text_touch_analog_pin}; // X-axis-signal
+	int TOUCH_DIGITAL_PIN = ${text_touch_digital_pin}; // Button
+  	#END
+
+	#SETUP
+	  pinMode (TOUCH_ANALOG_PIN, INPUT);
+	  pinMode (TOUCH_DIGITAL_PIN, INPUT);
+	#END
+	    
+	  // Current value will be read and converted to the voltage
+	  float ${variable_touch_analog_value} = analogRead (TOUCH_ANALOG_PIN) * (5.0 / 1023.0); 
+	  int ${variable_touch_digital_value} = digitalRead (TOUCH_DIGITAL_PIN);
+	  
+	  delay (200);
+	
+    `;
+    return code;
+  };
